4783abe115a956abf55455af2371843fa44c3462
ishiiairu <167832900+ishiiairu@users.noreply.github.com>
2024-07-02 09:30:42 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,9 @@
 alert('Hello, Airu!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt(Number('2')+Number('3')));
+alert(Math.sqrt('2'+3));
+alert(Math.sqrt(Number('2')+3));
+
