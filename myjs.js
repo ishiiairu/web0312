fe5bded8f6234fc0b8642997ef11e5954b2be4ab
ishiiairu <167832900+ishiiairu@users.noreply.github.com>
2024-07-29 11:20:06 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,2 @@
-alert('Hello, Airu!');
-alert(1+1);
-alert(Math.sqrt(1+1));
-alert(Math.sqrt(2))+alert(Math.sqrt(3));
-alert(Math.sqrt(1/0));
-alert(Math.sqrt(0/0));
-alert(Math.sqrt(2+3));
-alert(Math.sqrt('2'+'3'));
-alert(Math.sqrt(Number('2')+Number('3')));
-alert(Math.sqrt('2'+3));
-alert(Math.sqrt(Number('2')+3));
+alert('かわいい家族の写真です！');
 
